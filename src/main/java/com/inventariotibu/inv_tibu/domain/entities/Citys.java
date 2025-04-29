32138141ecd491456e1fe6afb91f67cb4ca933ef
ElidallanaCristancho <cristanchodayana062017@gmail.com>
2025-04-29 11:06:10 -0500

    feat: :sparkles: crear entidad Citys y actualizar relación en Region

--- src/main/java/com/inventariotibu/inv_tibu/domain/entities/Citys.java
+++ src/main/java/com/inventariotibu/inv_tibu/domain/entities/Citys.java
@@ -1,46 +1,35 @@
 package com.inventariotibu.inv_tibu.domain.entities;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Data
 @Getter
 @Setter
-@Table(name = "regions")
+@Table(name = "cities")
 @Entity
-public class Region {
+public class Citys {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
 
-    @Column(name = "name_region", length = 50, nullable = false, unique = true)
-    String name;
-    
-    @OneToMany(mappedBy = "regions",fetch = FetchType.LAZY,cascade = CascadeType.ALL)
-    @JsonManagedReference
-    private Set<Citys> cities = new HashSet<>();
+    @Column(name = "name_cities", length = 50, nullable = false, unique = true)
+    String namecity;
 
     @ManyToOne
-    @JoinColumn(name = "country_id")
+    @JoinColumn(name = "region_id")
     @JsonBackReference
-    Country countries;
+    Region regions;
 }
